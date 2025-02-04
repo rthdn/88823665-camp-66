9bcd6d579468e2d3a1a6c8da97cf098f4a6de0b7
rthdn <thakdanaimakmi@gmail.com>
2025-02-04 17:59:39 +0700

    Laravel - User Model

--- mylaravel/resources/views/user/edit.blade.php
+++ mylaravel/resources/views/user/edit.blade.php
@@ -4,20 +4,23 @@
     <div class="register-page">
         <div class="register-box">
             <div class="register-logo">
-                <a href="../index2.html"><b>Admin</b>LTE</a>
+                <a href=""><b>Edit</b>User</a>
             </div>
             <!-- /.register-logo -->
             <div class="card">
                 <div class="card-body register-card-body">
-                    <p class="register-box-msg">Register a new membership</p>
-                    <form action="{{ url('/register') }}" method="post">
+                    <p class="register-box-msg">Edit User</p>
+                    <form action="{{ url('/user') }}" method="post">
                         @csrf
+                        @method('put')
+                        <input type="hidden" name="id"
+                            value="{{ $user->id }}">
                         <div class="input-group mb-3">
-                            <input type="text" name="name" class="form-control" placeholder="Full Name" />
+                            <input type="text" value="{{ $user->name }}" name="name" class="form-control" placeholder="Full Name" />
                             <div class="input-group-text"><span class="bi bi-person"></span></div>
                         </div>
                         <div class="input-group mb-3">
-                            <input type="email" name="email" class="form-control" placeholder="Email" />
+                            <input type="email" value="{{ $user->email }}" name="email" class="form-control" placeholder="Email" />
                             <div class="input-group-text"><span class="bi bi-envelope"></span></div>
                         </div>
                         <div class="input-group mb-3">
@@ -37,7 +40,7 @@
                             <!-- /.col -->
                             <div class="col-4">
                                 <div class="d-grid gap-2">
-                                    <button type="submit" class="btn btn-primary">Sign In</button>
+                                    <button type="submit" class="btn btn-primary">Save</button>
                                 </div>
                             </div>
                             <!-- /.col -->
--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -5,10 +5,11 @@ use App\Http\Controllers\MyController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\UserController;
 
-Route::get('/test500', function () {
+/* Route::get('/test500', function () {
     abort(500);
-});
+}); test หน้า error 500 */
 
 Route::get('/login',
     [LoginController::class, 'index']);
@@ -18,6 +19,16 @@ Route::get('/home',
     [HomeController::class, 'index']);
 Route::get('/',
     [HomeController::class, 'index']);
+
+Route::post('/register',
+    [RegisterController::class, 'create']);
+
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/user/{id}', [UserController::class, 'edit']);
+Route::put('/user', [UserController::class, 'edit_action']);
+Route::delete('/user', [UserController::class, 'delete']);
+
+
 
 Route::get('/mycontroller/{id?}',
     [MyController::class, 'myfunction']);
